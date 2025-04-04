eec31150e3bb946501d3dd2336f19100c8ebe500
Somadina <kingswatter@gmail.com>
2025-04-04 11:08:54 +0100

    types

---
+++ src/types/index.d.ts
@@ -0,0 +1,31 @@
+export interface User {
+	id: string;
+	name: string;
+	avatar: string;
+}
+
+export interface Card {
+	id: string;
+	number: string;
+	expiryDate: string;
+	cardHolder: string;
+	type: 'mastercard' | 'visa';
+	balance: number;
+	color: 'blue' | 'dark-blue';
+}
+
+export interface Transaction {
+	id: string;
+	title: string;
+	amount: number;
+	date: Date;
+	category: 'entertainment' | 'food' | 'investment' | 'transfer' | 'other';
+	recipient?: string;
+	status: 'completed' | 'processing' | 'failed';
+}
+
+export interface ExpenseCategory {
+	category: string;
+	amount: number;
+	color: string;
+}
